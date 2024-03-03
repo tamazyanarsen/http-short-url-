25b66967fb71fa4b99b45975350ab266ff941c04
Arsen Tamazyan <atamazyan@t1-consulting.ru>
2024-03-03 12:34:19 +0300

    feat: POST метод для преобразования урла

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,40 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+			return
+		}
+		if r.Method == http.MethodPost {
+			body, err := io.ReadAll(r.Body)
+			println(string(body), err)
+			w.Header().Add("content-type", "text/plain")
+			w.WriteHeader(201)
+			w.Write([]byte("sdfsdfsdf"))
+		}
+	})
+	mux.HandleFunc("/{url}", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
+			return
+		}
+		for k, v := range r.URL.Query() {
+			println(k, v)
+		}
+		w.Header().Add("content-type", "text/plain")
+		w.Header().Add("Location", "sdfsdfdsf")
+		w.WriteHeader(301)
+		w.Write([]byte(""))
+	})
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
