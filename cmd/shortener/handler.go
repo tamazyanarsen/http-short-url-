ace0c6541dc205a8d7603e1d70a83bdd1146159e
Arsen Tamazyan <tamazyanarsen@mail.ru>
2024-03-05 08:43:41 +0300

    refactor: вынесен обработчик запросов

---
+++ cmd/shortener/handler.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"strings"
+)
+
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		shortUrl := strings.Split(r.URL.Path, "/")[1]
+		println(shortUrl)
+
+		w.Header().Add("content-type", "text/plain")
+		w.Header().Add("Location", "sdfsdfdsf")
+		w.WriteHeader(301)
+		w.Write([]byte(""))
+		return
+	}
+	if r.Method == http.MethodPost {
+		body, err := io.ReadAll(r.Body)
+		println(string(body), err)
+		w.Header().Add("content-type", "text/plain")
+		w.WriteHeader(201)
+		w.Write([]byte("sdfsdfsdf"))
+		return
+	}
+	http.Error(w, "now allowed method", http.StatusMethodNotAllowed)
+	return
+}
