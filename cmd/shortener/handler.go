bc8e3db59b18d4998f02466dafae1cf6c6e03809
Arsen Tamazyan <tamazyanarsen@mail.ru>
2024-03-08 20:39:54 +0300

    fix(main handler): код ответа для GET/

--- cmd/shortener/handler.go
+++ cmd/shortener/handler.go
@@ -9,11 +9,9 @@ import (
 func MainHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		shortURL := strings.Split(r.URL.Path, "/")[1]
-		println(shortURL)
-
 		w.Header().Add("content-type", "text/plain")
-		w.Header().Add("Location", "sdfsdfdsf")
-		w.WriteHeader(http.StatusTemporaryRedirect)
+		w.Header().Add("Location", shortURL)
+		w.WriteHeader(307)
 		w.Write([]byte(""))
 		return
 	}
@@ -21,8 +19,8 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
 		body, err := io.ReadAll(r.Body)
 		println(string(body), err)
 		w.Header().Add("content-type", "text/plain")
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte("sdfsdfsdf"))
+		w.WriteHeader(201)
+		w.Write([]byte("test-short-url"))
 		return
 	}
 	http.Error(w, "now allowed method", http.StatusMethodNotAllowed)
