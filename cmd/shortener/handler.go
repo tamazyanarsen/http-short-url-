c44fc9a394bbf0aebff66f5f04b24c4bde394cef
Arsen Tamazyan <tamazyanarsen@mail.ru>
2024-03-10 18:13:57 +0300

    fix: сохранение урлов

--- cmd/shortener/handler.go
+++ cmd/shortener/handler.go
@@ -1,27 +1,35 @@
 package main
 
 import (
+	"encoding/base64"
 	"io"
 	"net/http"
+	"regexp"
 	"strings"
 )
 
+var urls = make(map[string]string)
+
 func MainHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
+	switch r.Method {
+	case http.MethodGet:
 		shortURL := strings.Split(r.URL.Path, "/")[1]
+		println("shorturl", shortURL, len(urls), urls[shortURL])
 		w.Header().Add("content-type", "text/plain")
-		w.Header().Add("Location", shortURL)
-		w.WriteHeader(307)
-		w.Write([]byte(""))
-		return
-	}
-	if r.Method == http.MethodPost {
+		w.Header().Add("Location",
+			urls[regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(shortURL, "")])
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	case http.MethodPost:
 		body, err := io.ReadAll(r.Body)
 		println(string(body), err)
 		w.Header().Add("content-type", "text/plain")
-		w.WriteHeader(201)
-		w.Write([]byte("test-short-url"))
-		return
+		w.WriteHeader(http.StatusCreated)
+		short := base64.StdEncoding.EncodeToString(body)[:8]
+		shortURL := regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(short, "")
+		urls[shortURL] = string(body)
+		println("save", shortURL, "to map; result:", urls[shortURL])
+		w.Write([]byte("http://localhost:8080/" + shortURL))
+	default:
+		http.Error(w, "not allowed method", http.StatusMethodNotAllowed)
 	}
-	http.Error(w, "now allowed method", http.StatusMethodNotAllowed)
 }
