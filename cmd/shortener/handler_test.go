5fda8423a05e71031c462e79515dbc8dc90d9689
Arsen Tamazyan <atamazyan@t1-consulting.ru>
2024-03-10 16:27:43 +0300

    test(handler_test): правильный handler для тестов

--- cmd/shortener/handler_test.go
+++ cmd/shortener/handler_test.go
@@ -2,50 +2,72 @@ package main
 
 import (
 	"bytes"
+	"github.com/go-chi/chi"
+	"github.com/go-resty/resty/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestMainHandler(t *testing.T) {
-	type args struct {
-		//w http.ResponseWriter
-		w            *httptest.ResponseRecorder
-		r            *http.Request
-		want         string
-		expectedCode int
-	}
+type args struct {
+	w            *httptest.ResponseRecorder
+	method       string
+	url          string
+	body         io.Reader
+	want         string
+	expectedCode int
+}
+
+func TestHandler(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
 	}{
 		// TODO: Add test cases.
 		{name: "get test", args: args{
-			w:            httptest.NewRecorder(),
-			r:            httptest.NewRequest(http.MethodGet, "/test", nil),
+			method:       http.MethodGet,
+			url:          "/test",
+			body:         nil,
 			want:         "test",
 			expectedCode: http.StatusTemporaryRedirect,
 		}},
 		{name: "post test", args: args{
-			w:            httptest.NewRecorder(),
-			r:            httptest.NewRequest(http.MethodPost, "/", bytes.NewReader([]byte("some text"))),
+			method:       http.MethodPost,
+			url:          "/",
+			body:         bytes.NewReader([]byte("some text")),
 			want:         "test-short-url",
 			expectedCode: http.StatusCreated,
 		}},
 	}
+	r := chi.NewRouter()
+	r.Get("/{short}", MainHandler)
+	r.Post("/", MainHandler)
+
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			MainHandler(tt.args.w, tt.args.r)
+			client := resty.New().R()
+			client.URL = ts.URL + tt.args.url
+			client.Method = tt.args.method
+			client.SetBody(tt.args.body)
+			res, err := client.Send()
+			if err != nil {
+			}
+			println(res.StatusCode(), res.Header().Get("Location"))
+
 			data := ""
-			if tt.args.r.Method == http.MethodGet {
-				data = tt.args.w.Header().Get("Location")
+			if tt.args.method == http.MethodGet {
+				data = res.Header().Get("Location")
 			}
-			if tt.args.r.Method == http.MethodPost {
-				data = tt.args.w.Body.String()
+			if tt.args.method == http.MethodPost {
+				data = string(res.Body())
 			}
-			require.Equal(t, tt.args.expectedCode, tt.args.w.Code, "Неверный код ответа")
+			require.Equal(t, tt.args.expectedCode, res.StatusCode(), "Неверный код ответа")
 			assert.Equal(t, tt.args.want, data)
 		})
 	}
--- go.mod
+++ go.mod
@@ -3,16 +3,17 @@ module http-short-url
 go 1.21.5
 
 require (
-	github.com/go-chi/chi/v5 v5.0.12
+	github.com/go-chi/chi v1.5.5
+	github.com/go-resty/resty/v2 v2.11.0
 	github.com/stretchr/testify v1.9.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-chi/chi v1.5.5 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
