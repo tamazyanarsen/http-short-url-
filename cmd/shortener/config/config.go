753c59906fa30a7aa1d9bfc98e88df17aa38bd13
Arsen Tamazyan <atamazyan@t1-consulting.ru>
2024-04-21 06:57:09 +0300

    feat(config): настройка запуска через переменные окружения

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -1,10 +1,19 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var Config = make(map[string]*string)
 
 func init() {
 	Config["a"] = flag.String("a", "localhost:8080", "адрес запуска HTTP-сервера")
 	Config["b"] = flag.String("b", "http://localhost:8080/", "базовый адрес результирующего сокращённого url")
+	if serverAddr := os.Getenv("SERVER_ADDRESS"); serverAddr != "" {
+		Config["a"] = &serverAddr
+	}
+	if baseAddr := os.Getenv("BASE_URL"); baseAddr != "" {
+		Config["b"] = &baseAddr
+	}
 }
