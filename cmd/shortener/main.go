8edb85d6668d15d5b9ffd38c10aad87d9da0cd7a
Arsen Tamazyan <atamazyan@nota.tech>
2024-05-27 12:30:09 +0300

    fix

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -15,7 +15,7 @@ func main() {
 	println(*config.Config["a"], *config.Config["b"])
 	r := chi.NewRouter()
 	r.Get("/{short}", handler.WithLog(handler.GzipHandler(handler.GetShort)))
-	r.Post("/", handler.WithLog(handler.PostURL))
+	r.Post("/", handler.WithLog(handler.GzipHandler(handler.PostURL)))
 	r.Post("/api/shorten", handler.WithLog(handler.GzipHandler(handler.PostJSON)))
 	err := http.ListenAndServe(*config.Config["a"], r)
 	if err != nil {
